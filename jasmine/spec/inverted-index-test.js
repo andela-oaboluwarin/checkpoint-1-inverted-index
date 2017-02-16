1153f3116a7136c9836f11b60cb20ea471cb9c3c
Oreoluwa Aboluwarin <oreoluwa.aboluwarin@andela.com>
2017-02-16 19:48:15 +0100

    feature(test): add test specs to test for negatives

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -1,13 +1,16 @@
-const goodBooks = require('../goodBooks.json');
+const briefContent = require('../briefContent');
+const weirdFormat = require('../weirdFormat');
+const goodBooks = require('../goodBooks');
 
 // Inverted Index test suite
 describe('Inverted Index Suite', () => {
   // Define variables to be used in the test Suite
   const newIndex = new InvertedIndex();
-  newIndex.createIndex('goodBooks', goodBooks);
+
   const sampleSentence =
     'This be an %example of the #typical be sentence type.';
   const multipleSearch = 'Destroy world imagination quickly.';
+  const arraySearch = '[forever, platitude] may be great.';
 
   // Inverted Index class test suite
   describe('Class Inverted Index', () => {
@@ -15,20 +18,38 @@ describe('Inverted Index Suite', () => {
       expect(newIndex instanceof InvertedIndex).toBe(true);
       expect(newIndex instanceof Object).toBe(true);
       expect(typeof (newIndex)).toBe('object');
+      expect(typeof (newIndex)).not.toBe('string');
+      expect(newIndex instanceof Object).not.toBe(false);
+    });
+
+    it('should return false if the json file is invalid or empty', () => {
+      const isValid = InvertedIndex.isValidFile(weirdFormat);
+      expect(isValid).toBe(false);
+    });
+
+
+    it('should return true if file format is as expected', () => {
+      const isValid = InvertedIndex.isValidFile(goodBooks);
+      expect(isValid).toBe(true);
     });
   });
+
+
   // Get Tokens test Suite
   describe('Get Tokens String', () => {
     it('should be available in class InvertedIndex', () => {
       expect(InvertedIndex.getTokens).toBeDefined();
     });
+
     it('should return an array containing alphabets only', () => {
       expect(InvertedIndex.getTokens(sampleSentence)).not.toContain('%');
     });
+
     it('should return an array containing the correct number of words', () => {
       expect(InvertedIndex.getTokens(sampleSentence).length).toBe(10);
     });
   });
+
 
   // Book Data Test Suite
   describe('Read Book Data', () => {
@@ -41,22 +62,28 @@ describe('Inverted Index Suite', () => {
     it('should be present in class InvertedIndex', () => {
       expect(InvertedIndex.distinctWords).toBeDefined();
     });
+
     it('should return an array of words without duplicates', () => {
       expect(InvertedIndex.distinctWords(sampleSentence).length).toBe(9);
     });
   });
 
   describe('Populate Index', () => {
+    newIndex.createIndex('briefContent', briefContent);
     it('should have an Index created', () => {
       expect(newIndex.index).toBeDefined();
     });
+
     it('should accurately map words to their document location', () => {
-      expect(newIndex.index.goodBooks.a).toEqual([0, 1]);
-      expect(newIndex.index.goodBooks.wonderland).toEqual([0]);
+      expect(newIndex.index.briefContent.and).toEqual([0]);
+      expect(newIndex.index.briefContent.weapons).toEqual([0]);
+      expect(newIndex.index.briefContent.avatar).not.toBe([0]);
+      expect(newIndex.index.briefContent.cutlasses).not.toBe([1]);
     });
   });
 
   describe('Get Index', () => {
+    newIndex.createIndex('goodBooks', goodBooks);
     it('should return an accurate index Object of the indexed file', () => {
       expect(newIndex.getIndex('goodBooks').alice).toEqual([0]);
       expect(Object.keys(newIndex.getIndex('goodBooks')).length).toBe(29);
@@ -64,9 +91,11 @@ describe('Inverted Index Suite', () => {
   });
 
   describe('Search Index', () => {
+    newIndex.createIndex('goodBooks', goodBooks);
     it('should have searchIndex method accessible in the class', () => {
       expect(newIndex.searchIndex).toBeDefined();
     });
+
     it('should return correct index document for each word', () => {
       expect(newIndex.searchIndex('fellowship', 'goodBooks'))
         .toEqual({ fellowship: [1] });
@@ -74,7 +103,11 @@ describe('Inverted Index Suite', () => {
         of: [0, 1]
       });
       expect(newIndex.searchIndex('discombobulated', 'goodBooks'))
-        .toEqual({ discombobulated: {} });
+        .toEqual({
+          discombobulated: {}
+        });
+      expect(newIndex.searchIndex('ring', 'goodBooks'))
+        .not.toBe({ ring: [0] });
       expect(newIndex.searchIndex(multipleSearch,
         'goodBooks')).toEqual({
           destroy: [1],
@@ -83,6 +116,17 @@ describe('Inverted Index Suite', () => {
           quickly: {}
         });
     });
+
+    it('should return search result if an array is passed as term', () => {
+      expect(newIndex.searchIndex(arraySearch,
+        'goodBooks')).toEqual({
+          fellowship: [1],
+          ring: [1]
+        });
+      expect(newIndex.searchIndex(arraySearch, 'goodBooks')).not.toBe({
+        fellowship: [0],
+        ring: [0]
+      });
+    });
   });
 });
-
